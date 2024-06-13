3440fb49cc2054ced5d6592970e45e4c4c0839a1
phhoangloc <82487565+phhoangloc@users.noreply.github.com>
2024-06-13 17:14:56 +0900

    0613

--- src/pages/api/auth/user.tsx
+++ src/pages/api/auth/user.tsx
@@ -18,29 +18,33 @@ export default async function handler(
     const authorization = req.headers['authorization']
     const token = authorization && authorization.split(" ")[1]
     // const id = token && await jwt.verify(token, 'secretToken').id
-    token && await jwt.verify(token, 'secretToken', async (err: Error, data: any) => {
-        if (err) {
-            if (err.name === 'TokenExpiredError') {
-                result.message = 'Phiên đăng nhập đã hết hạn, vui lòng đăng nhập lại'
-                res.json(result);
+    if (token) {
+        await jwt.verify(token, 'secretToken', async (err: Error, data: any) => {
+            if (err) {
+                if (err.name === 'TokenExpiredError') {
+                    result.message = 'Phiên đăng nhập đã hết hạn, vui lòng đăng nhập lại'
+                    res.json(result);
+                } else {
+                    result.message = 'đăng nhập không hợp lệ, vui lòng đăng nhập lại'
+                    res.json(result);
+                }
             } else {
-                result.message = 'đăng nhập không hợp lệ, vui lòng đăng nhập lại'
-                res.json(result);
+                id = data.id
+                await userModel
+                    .findOne({ "_id": id })
+                    .catch((error: Error) => {
+                        result.success = false
+                        result.message = error.message
+                        res.send(result)
+                        throw error.message
+                    }).then(async (data: any) => {
+                        result.success = true
+                        result.data = data
+                        res.json(result)
+                    })
             }
-        } else {
-            id = data.id
-            await userModel
-                .findOne({ "_id": id })
-                .catch((error: Error) => {
-                    result.success = false
-                    result.message = error.message
-                    res.send(result)
-                    throw error.message
-                }).then(async (data: any) => {
-                    result.success = true
-                    result.data = data
-                    res.json(result)
-                })
-        }
-    })
+        })
+    } else {
+        res.send(result)
+    }
 }
