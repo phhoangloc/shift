bec8f29b0db27686afff73c18e2b6d39ec445d04
phhoangloc <82487565+phhoangloc@users.noreply.github.com>
2024-06-13 17:49:09 +0900

    0613

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,14 @@
 import { NextResponse } from 'next/server'
 import { NextApiRequest } from 'next';
 
-const allowedIPs = ['192.168.1.1'];
+const allowedIPs = ['122.209.142.173'];
 
 export default function middleware(req: NextApiRequest) {
 
-    if (!allowedIPs.includes('192.168.1.1')) {
+    const ip: string | string[] | undefined = req.headers['x-forwarded-for']
+    console.log(ip)
+
+    if (!allowedIPs.includes('122.209.142.173')) {
         return NextResponse.redirect(new URL('/error', req.url))
     } else {
         return NextResponse.next()
