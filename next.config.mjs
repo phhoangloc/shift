efec83e28d75cb42576f592303444e03afbbdbc8
unknown <ph.hoangloc@gmail.com>
2024-06-25 10:53:48 +0900

    0625

--- next.config.mjs
+++ next.config.mjs
@@ -15,7 +15,7 @@ const nextConfig = {
         // MONGODB_URL: "mongodb://h-loc:6pS3qvpeUQxuubKi@ac-5djtpn4-shard-00-00.qs9wtoh.mongodb.net:27017,ac-5djtpn4-shard-00-01.qs9wtoh.mongodb.net:27017,ac-5djtpn4-shard-00-02.qs9wtoh.mongodb.net:27017/bulletin?ssl=true&replicaSet=atlas-d75adp-shard-0&authSource=admin&retryWrites=true&w=majority",
         HOMEPAGE_URL: "https://shift-xi.vercel.app/",
         HOMEPAGE_URL_: "http://localhost:3000/",
-        FTP_URL: "http://www.locpham.free.nf/",
+        FTP_URL: "http://locpham.free.nf/",
     }
 };
 
