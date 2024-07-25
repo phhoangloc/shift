04eb1b5442ae38839559b593139053ef821042ea
unknown <ph.hoangloc@gmail.com>
2024-07-25 13:43:05 +0900

    0725

--- src/redux/component/provider.tsx
+++ src/redux/component/provider.tsx
@@ -30,8 +30,6 @@ const Provider = ({ children }: Props) => {
     const [loading, setLoading] = useState<boolean>(true)
     const checkLogin = async () => {
         setLoading(true)
-        await NoUser.getItem({ genre: "category" })
-        await NoUser.getItem({ genre: "pic" })
         const result = await UserAuthen.checkLogin()
         if (result.success) {
             store.dispatch(setUser(result.data))
@@ -46,6 +44,10 @@ const Provider = ({ children }: Props) => {
         checkLogin()
     }, [currentRefresh])
 
+    useEffect(() => {
+        NoUser.getItem({ genre: "category" })
+        NoUser.getItem({ genre: "pic" })
+    }, [])
     return (
         loading ? <div style={{ width: "100vw", height: "100vh" }}><Loading /></div> :
             <>
